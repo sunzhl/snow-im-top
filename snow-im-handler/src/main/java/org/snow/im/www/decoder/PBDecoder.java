5b84df9ad2b753d52345cdb965e396f0e59c2184
sun_zhl <sun_zhl7981@163.com>
2018-10-18 23:17:19 +0800

    创建基础的类

---
+++ snow-im-handler/src/main/java/org/snow/im/www/decoder/PBDecoder.java
@@ -0,0 +1,9 @@
+package org.snow.im.www.decoder;
+
+/**
+ * @Auther admin
+ * @Date 2018/10/18
+ * @Description
+ */
+public class PBDecoder {
+}
---
+++ snow-im-handler/src/main/java/org/snow/im/www/encoder/PBEncoder.java
@@ -0,0 +1,9 @@
+package org.snow.im.www.encoder;
+
+/**
+ * @Auther admin
+ * @Date 2018/10/18
+ * @Description
+ */
+public class PBEncoder {
+}
